78b68e7f5b408470db04c0a287f249ec569598ce
Rohith <rohith@Rohiths-MacBook-Pro.local>
2023-10-09 17:33:40 +0100

    changefont

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'Lexend': ['Lexend', 'sans-serif'],
+        'VT323': ['VT323', 'monospace']
+      },
     },
   },
   plugins: [],
