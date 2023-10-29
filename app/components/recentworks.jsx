c52b4429d1af7fc65b6145ffcd6df33736309031
Rohith <rohith@Rohiths-MacBook-Pro.local>
2023-10-29 13:13:52 +0000

    urgent

--- app/components/recentworks.jsx
+++ app/components/recentworks.jsx
@@ -18,9 +18,10 @@ const Works = async () => {
   const data = (await getData()) || [];
 
   return (
+    
     <div className="mt-4 grid lg:grid-cols-2 gap-4 md:grid-cols-2 grid-cols-1">
       {data.map((design) => (
-     
+     <a href={design.link} className="text-xl font-extralight text-blue-300">
         <div key={design._id}>
             
           <div className="mt-20" >
@@ -35,10 +36,11 @@ const Works = async () => {
             />)}
             <h1 className="font-bold font-VT323 mt-8 lg:text-2xl md:text-xl text-xl">{design.heading}</h1>
             <p className="font-light font-VT323 mt-2 mb-4 text-sm lg:text-xl md:text-lg text-gray-400">{design.description}</p>
-            <a href={design.link} className="text-xl font-extralight text-blue-300">Read Blog</a>
+            
           
           </div>
         </div>
+        </a>
       ))}
     </div>
     
