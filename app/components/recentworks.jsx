10ec0939f76a3c2e01b32da5989485dd181ccb70
Rohith <rohith@Rohiths-MacBook-Pro.local>
2023-10-29 13:19:20 +0000

    cgabge z3

--- app/components/recentworks.jsx
+++ app/components/recentworks.jsx
@@ -16,28 +16,22 @@ async function getData() {
 
 const Works = async () => {
   const data = (await getData()) || [];
-
   return (
     
     <div className="mt-4 grid lg:grid-cols-2 gap-4 md:grid-cols-2 grid-cols-1">
       {data.map((design) => (
      <a href={design.link} className="text-xl font-extralight text-blue-300">
-        <div key={design._id}>
-            
+        <div key={design._id}>  
           <div className="mt-20" >
-            
           {design.imageUrl && (
             <Image className="w-auto h-lg" style={{ height: '70%', width: '100%' }}
            width={1400}
            height={500}
-
               src={design.imageUrl}
               alt={design.heading}
             />)}
             <h1 className="font-bold font-VT323 mt-8 lg:text-2xl md:text-xl text-xl">{design.heading}</h1>
             <p className="font-light font-VT323 mt-2 mb-4 text-sm lg:text-xl md:text-lg text-gray-400">{design.description}</p>
-            
-          
           </div>
         </div>
         </a>
--- app/page.js
+++ app/page.js
@@ -19,8 +19,8 @@ export default function Home() {
       <div className='container  mx-auto p-4 bg-black'>
         <div className='grid lg:grid-cols-2 gap-4 mt-16 sm:grid-cols-1 md:grid-cols-2  text-grey-900'>
           <div className='mt-0 lg:mt-60 md:mt-52' data-aos="fade-right">
-            <h1 className="font-bold text-4xl lg:text-5xl font-VT323 pb-4 ">Hey ! &#128591;</h1>
-            <h1 className="font-bold text-4xl lg:text-5xl font-VT323">I’m Rohith Nair</h1>
+            <h1 className="font-bold text-4xl lg:text-5xl font-VT323 pb-4 ">Hey! &#128591;</h1>
+            <h1 className="font-bold text-4xl lg:text-5xl font-VT323">I’m Rohith Nair Reghu</h1>
             <h2 className="text-2xl pt-2 d font-VT323">Senior Product Designer, Developer </h2>
             <p className="pt-5 pb-14 max-w-l font-thin font-VT323 text-l text-gray-400">With four years of experience in the field of designing and development, I'm committed to uniting people and technology, harnessing its potential to empower individuals, and driving positive change in society.</p>
             <a className="text-xl text-blue-500 mt-8 font-VT323 font-semibold underline" href="mailto:rohithnairreghu@gmail.com">Get in Touch &#8594;</a>
